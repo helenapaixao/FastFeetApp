9041fc0f7c0ea46d7ed891fde0aa39c0e85c2ae0
Helena Paixão <hp.helenapaixao@gmail.com>
2020-12-08 20:04:35 -0400

    config text in card

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -2,4 +2,16 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   background-color: #5433;
+  width: 327px;
+  height: 184px;
+  border-radius: 4px;
+  margin-left: 30px;
+  margin-top: 50px;
+`;
+
+export const Title = styled.Text`
+  color: #4c4766;
+  font-family: 'Roboto Condensed';
+  font-weight: 700;
+  font-size: 22px;
 `;
